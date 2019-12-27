a1f556fd8763a41429abfb5e1839e476b07881da
Jason <nj.ho@hotmail.com>
2019-12-26 21:05:07 -0700

    Checkpoint Before Running Deep Sort

--- blink-detection/detect_blinks.py
+++ blink-detection/detect_blinks.py
@@ -18,12 +18,12 @@ import cv2
 def eye_aspect_ratio(eye):
     # compute the euclidean distances between the two sets of
     # vertical eye landmarks (x, y)-coordinates
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
+    A = dist.euclidean(eye[13], eye[19])
+    B = dist.euclidean(eye[15], eye[17])
 
     # compute the euclidean distance between the horizontal
     # eye landmark (x, y)-coordinates
-    C = dist.euclidean(eye[0], eye[3])
+    C = dist.euclidean(eye[12], eye[16])
 
     # compute the eye aspect ratio
     ear = (A + B) / (2.0 * C)
@@ -43,8 +43,8 @@ args = vars(ap.parse_args())
 # define two constants, one for the eye aspect ratio to indicate
 # blink and then a second constant for the number of consecutive
 # frames the eye must be below the threshold
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 3
+EYE_AR_THRESH = 0.1
+EYE_AR_CONSEC_FRAMES = 1
 
 # initialize the frame counters and the total number of blinks
 COUNTER = 0
@@ -58,8 +58,11 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 
 # grab the indexes of the facial landmarks for the left and
 # right eye, respectively
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+print(lStart, lEnd)
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+print(rStart, rEnd)
 
 # start the video stream thread
 print("[INFO] starting video stream thread...")
@@ -98,23 +101,23 @@ while True:
         # extract the left and right eye coordinates, then use the
         # coordinates to compute the eye aspect ratio for both eyes
         leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
+        # rightEye = shape[rStart:rEnd]
         leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
+        # rightEAR = eye_aspect_ratio(rightEye)
 
         # average the eye aspect ratio together for both eyes
-        ear = (leftEAR + rightEAR) / 2.0
+        ear = (leftEAR) / 1.0
 
         # compute the convex hull for the left and right eye, then
         # visualize each of the eyes
         leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
+        # rightEyeHull = cv2.convexHull(rightEye)
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        # cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
         # check to see if the eye aspect ratio is below the blink
         # threshold, and if so, increment the blink frame counter
-        if ear < EYE_AR_THRESH:
+        if ear > EYE_AR_THRESH:
             COUNTER += 1
 
         # otherwise, the eye aspect ratio is not below the blink
@@ -132,7 +135,7 @@ while True:
         # the computed eye aspect ratio for the frame
         cv2.putText(
             frame,
-            "Blinks: {}".format(TOTAL),
+            "Talking Points: {}".format(TOTAL),
             (10, 30),
             cv2.FONT_HERSHEY_SIMPLEX,
             0.7,
